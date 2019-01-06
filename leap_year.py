7b8031b6cbdbec5311597ebdd1556bce72580c08
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-01-06 23:56:07 +0100

    improvements

--- leap_year.py
+++ leap_year.py
@@ -1,17 +1,7 @@
-year = int(input('Please enter a year:\n> '))
-leap_year = False
+while True:
+  year = int(input('\nPlease enter a year:\n(or press [ctrl + C] to quit)\n> '))
 
-if (year % 400 == 0):
-  leap_year = True
-elif (year % 100 == 0):
-  leap_year = False
-elif (year % 4 == 0):
-  leap_year = True
-else:
-  leap_year = False
-
-
-if (leap_year == True):
-  print('The year %d is a leap year.' %(year))
-else:
-  print('The year %d is not a leap year.' %(year))
+  if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    print('The year %d is a leap year.' %(year))
+  else:
+    print('The year %d is not a leap year.' %(year))
