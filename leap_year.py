f06f916f214e73eee017f17dee9f98326a75279b
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-01-08 16:47:33 +0100

    exceptions

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,19 @@
-while True:
-  year = int(input('\nPlease enter a year:\n(or press [ctrl + C] to quit)\n> '))
+def leap_year():
+  while True:
+    year = input('\nPlease enter a year superior than 0:\n(or press [ctrl + C] to quit)\n> ')
+    try:
+      year = int(year)
+      assert year > 0
+    except ValueError:
+      print("You did not enter a number.")
+      leap_year()
+    except AssertionError:
+      print("The year entered is less than or equal to 0.")
+      leap_year()
 
-  if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
-    print('The year %d is a leap year.' %(year))
-  else:
-    print('The year %d is not a leap year.' %(year))
+    if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+      print('The year %d is a leap year.' %(year))
+    else:
+      print('The year %d is not a leap year.' %(year))
+
+leap_year()
--- mult.py
+++ mult.py
@@ -2,7 +2,14 @@
 
 def multiplication_table():
   while True:
-    nb = int(input("\nPlease enter a number:\n> "))
+    nb = input("\nPlease enter a number:\n> ")
+    try:
+      nb = int(nb)
+    except ValueError:
+      print('The value must be an integer.')
+      multiplication_table()
+    else:
+      print("There is the multiplication table of " + str(nb) + ":\n")
 
     def perform(nb, max = 10):
       i = 0
