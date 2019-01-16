09c56e5cc0b1db8d781df8db733e1025c8f82513
Nikita Vasilev <vassilev.nikita@gmail.com>
2019-01-16 23:46:34 +0100

    tuples test

---
+++ tuples.py
@@ -0,0 +1,7 @@
+def decomposer(entier, divise_par):
+    """Cette fonction retourne la partie entière et le reste de
+    entier / divise_par"""
+
+    p_e = entier // divise_par
+    reste = entier % divise_par
+    return p_e, reste
